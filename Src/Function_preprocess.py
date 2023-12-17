9e60370b6816fe45cce46af4bc9dd589213d704c
YannickB <68849596+Yannick1977@users.noreply.github.com>
2023-12-17 12:28:03 +0100

    Mise a jour 'function preprocess'. Fichier data ok

--- Src/Function_preprocess.py
+++ Src/Function_preprocess.py
@@ -20,53 +20,111 @@ def preprocess(df_):
     num_cols = df_.select_dtypes(exclude='O').columns.to_list()
     num_cols.remove('Target')
 
+    # Drop the 'Target' column and rename the 'Failure Type' column to 'Target'
     df_.drop(columns=['Target'], inplace=True)
     df_.rename(columns={'Failure Type': 'Target'}, inplace=True)
 
     # Create a ColumnTransformer object
-    ct_ = ColumnTransformer([
-        ("onehot", OneHotEncoder(), cat_cols),
-        ("scale", StandardScaler(), num_cols),
-        ("fail_type", OrdinalEncoder(), ['Target'])
-    ])
+    ct_X_ = ColumnTransformer(
+        transformers=[
+            ("cat", OneHotEncoder(), cat_cols),
+            ("num", StandardScaler(), num_cols)
+            #("fail", OrdinalEncoder(), ['Target'])
+        ],
+        remainder='passthrough')
+    ct_y_ = ColumnTransformer(
+        transformers=[
+            ("fail", OrdinalEncoder(), ['Target'])
+        ],
+        remainder='passthrough')
 
-    # Fit and transform the DataFrame
-    df_ = ct_.fit_transform(df_)
-    df_ = pd.DataFrame(df_)
-    df_.columns = ct_.get_feature_names_out()
+    # Create different dataset for X train, test and validation
+
+    # Create dataset for training and validation
+    X_, X_val, y_, y_val = train_test_split(
+        df_.drop(columns=['Target']), 
+        df_['Target'], 
+        test_size=0.2,
+        stratify=df_['Target'],
+        random_state=42)
+
+    # Create dataset for training and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_, 
+        y_, 
+        test_size=0.2,
+        stratify=y_,
+        random_state=42)
+
+    # Fit and transform datasets
+    X_train_trans, X_test_trans, X_val_trans = fit_transform(ct_X_, X_train, X_test, X_val)
+    y_train_trans, y_test_trans, y_val_trans = fit_transform(ct_y_, pd.DataFrame(y_train), pd.DataFrame(y_test), pd.DataFrame(y_val))
 
     # Create a dictionary to map the target values to their corresponding indices
-    cat_=ct_.named_transformers_['fail_type'].categories_
+    cat_=ct_y_.named_transformers_['fail'].categories_
     dict_target_ = {}
     dict_target_inv_ = {}
     for i, c in enumerate(cat_[0]):
         dict_target_[c] = i
         dict_target_inv_[i] = c
 
-    # Create dataset for training and validation
-    X_, X_val, y_, y_val = train_test_split(
-        df_.drop(columns=['fail_type__Target']), 
-        df_['fail_type__Target'], 
-        test_size=0.2,
-        stratify=df_['fail_type__Target'],
-        random_state=42)
-    
     # Save the validation dataset
-    pd.concat([X_val, y_val], axis=1).to_csv('../data/predictive_maintenance_validation.csv')
+    #pd.concat([X_val_trans, y_val_trans], axis=1).to_csv('../data/predictive_maintenance_validation.csv')
+    create_df_from_X_y(X_=X_val_trans, y_=y_val_trans, columns_y=dict_target_inv_.values()).to_csv('../data/predictive_maintenance_validation.csv', index=False)
 
-    # Apply SMOTE to the training dataset
+    # Apply SMOTE to the dataset train and test
+    X_train_res_, y_train_res_ = apply_smote(X_=X_train_trans, y_=y_train_trans)
+    X_test_res_, y_test_res_ = apply_smote(X_=X_test_trans, y_=y_test_trans)
+
+
+    # Save the train and test dataset
+    df_train = create_df_from_X_y(X_=X_train_res_, y_=y_train_res_, columns_y=dict_target_inv_.values())
+    df_train.to_csv('../data/predictive_maintenance_train_resampled.csv', index=False)
+    df_test = create_df_from_X_y(X_=X_test_res_, y_=y_test_res_, columns_y=dict_target_inv_.values())
+    df_test.to_csv('../data/predictive_maintenance_test_resampled.csv', index=False)
+
+
+    return df_train, df_test, dict_target_inv_
+
+def fit_transform(ct_, train, test, val):
+
+    # Fit and transform the train_ dataset
+    train_trans_ = ct_.fit_transform(train)    # train_trans_ is a numpy array afetr this step
+    train_trans_ = pd.DataFrame(train_trans_)
+    train_trans_.columns = ct_.get_feature_names_out()
+
+    # Transform the test_ dataset
+    test_trans_ = ct_.transform(test)
+    test_trans_ = pd.DataFrame(test_trans_)
+    test_trans_.columns = ct_.get_feature_names_out()
+
+    # Transform the val_ dataset
+    val_trans_ = ct_.transform(val)
+    val_trans_ = pd.DataFrame(val_trans_)
+    val_trans_.columns = ct_.get_feature_names_out()
+
+    return train_trans_, test_trans_, val_trans_
+
+def apply_smote(X_,y_):
     smote = SMOTE(random_state=12)
     X_resampled_, y_resampled_ = smote.fit_resample(X_.to_numpy(), y_.to_numpy())
+
+    X_resampled_= pd.DataFrame(X_resampled_)
+    X_resampled_.columns = X_.columns
+    y_resampled_ = pd.DataFrame(y_resampled_)
+    y_resampled_.columns = y_.columns
+
+    return X_resampled_, y_resampled_
+
+def create_df_from_X_y(X_, y_, columns_y):
+
+    y_ = pd.DataFrame(pd.get_dummies(y_.astype(str) ))
+    y_.columns=list(columns_y)
+
+    return pd.concat([X_, y_], axis=1)
+
+
     
-    # Create a DataFrame from the resampled data
-    df_ = pd.DataFrame(X_resampled_, columns=X_.columns)
-    y_resampled_ = pd.DataFrame(pd.get_dummies(y_resampled_))
-    y_resampled_.columns=list(dict_target_inv_.values())
-    df_ = pd.concat([df_, y_resampled_], axis=1)
-
-    # Save the training dataset
-    df_.to_csv('../data/predictive_maintenance_train.csv', index=False)
-
-    return df_, dict_target_inv_
+    
 
 
